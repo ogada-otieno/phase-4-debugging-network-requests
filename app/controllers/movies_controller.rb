dddde96f6f8ffc6d9f27c4405e88a6487ea92bb4
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-16 12:33:21 +0300

    Done!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,4 +5,15 @@ class MoviesController < ApplicationController
     render json: movies
   end
 
+  def create
+    movie = Movie.new(movie_params)
+    render json: movie, status: 201
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
 end
